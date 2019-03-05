4fcf5f597ee2b9283bbc743576ccd6d6064967e4
bojan <bojan@DESKTOP-T6EMOUM>
2019-03-05 13:14:40 +0100

    na new new new dodadeno

--- config.php
+++ config.php
@@ -7,24 +7,3 @@
  */
 
 echo "hi from config";
-
-
-$get_lele = Yii::$app->request->get('lele', false);
-if (isset($get_lele) && $get_lele != false) {
-    $resultsTradeMakeTypeIn = new \app\models\Maker();
-    $rtmti = $resultsTradeMakeTypeIn->shortcodesFromResult($get_lele);
-
-    $sessionData['postedData']['Lead']['manufacturer'] = $rtmti['manufacturer'];
-    $mfk = $model->getKeyForMappingKey($_mkeys, 'manufacturer');
-    $sessionData['postedData']['Lead'][$mfk . '_id'] = $rtmti['manufacturer_id'];
-    $sessionData['postedData']['Lead'][$mfk] = $rtmti['manufacturer'];
-
-    $sessionData['postedData']['Lead']['year'] = $rtmti['year'];
-    $mfk = $model->getKeyForMappingKey($_mkeys, 'year');
-    $sessionData['postedData']['Lead'][$mfk] = $rtmti['year'];
-
-    $sessionData['postedData']['Lead']['model'] = $rtmti['model'];
-    $mfk = $model->getKeyForMappingKey($_mkeys, 'model');
-    $sessionData['postedData']['Lead'][$mfk . '_id'] = $rtmti['model_id'];
-    $sessionData['postedData']['Lead'][$mfk] = $rtmti['model'];
-}
